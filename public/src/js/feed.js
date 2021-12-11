959739207221c24fb21fd0670f97e0b72b13f433
Yotam Lifschytz <yotam.lif@gmail.com>
2021-12-11 21:25:05 +0200

    SW - done

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -4,6 +4,23 @@ var closeCreatePostModalButton = document.querySelector('#close-create-post-moda
 
 function openCreatePostModal() {
   createPostArea.style.display = 'block';
+  // From here - When to show installation prompt
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+
+    deferredPrompt.userChoice.then(function(choiceResult) {
+      console.log(choiceResult.outcome);
+
+      if (choiceResult.outcome === 'dismissed') {
+        console.log('User cancelled installation');
+      } else {
+        console.log('User added to home screen');
+      }
+    });
+
+    deferredPrompt = null;
+    // End
+  }
 }
 
 function closeCreatePostModal() {
